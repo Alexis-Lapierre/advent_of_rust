ca247b871fa7b5782cde881324dcee37cd93b7e8
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2023-11-28 22:59:48 +0100

    Split up code into different modules

---
+++ src/advent_of_code_2022.rs
@@ -0,0 +1,7 @@
+mod day4;
+pub fn advent_of_code_2022(day: u8) {
+    match day {
+        4 => day4::solve(),
+        _ => todo!(),
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 use std::env;
 
-fn main() {
-    let day = get_day(&env::args().nth(1));
+mod advent_of_code_2022;
+use advent_of_code_2022::advent_of_code_2022;
 
-    println!("Hello, world! Your day is {}", day);
+fn main() {
+    let (year, day) = get_year_and_day();
+
+    println!("Starting {}-{}", year, day);
+
+    match year {
+        2022 => advent_of_code_2022(day),
+        2023 => advent_of_code_2023(day),
+        _ => panic!("Unexpected year {}", year),
+    }
 }
 
-fn get_day(maybe_day: &Option<String>) -> u8 {
-    if let Some(day_str) = maybe_day {
-        day_str
-            .parse()
-            .expect("First given argument to be a string")
-    } else {
-        println!("No day given, defaulting to first day");
-        1
-    }
+fn advent_of_code_2023(day: u8) {
+    todo!()
+}
+
+fn get_year_and_day() -> (u16, u8) {
+    const USAGE: &'static str = "Usage: YEAR DAY";
+    const FORMAT: &'static str = "Expected a number";
+    let mut args = env::args();
+
+    let year: u16 = args.nth(1).expect(USAGE).parse().expect(FORMAT);
+    let day: u8 = args.nth(0).expect(USAGE).parse().expect(FORMAT);
+
+    (year, day)
 }
