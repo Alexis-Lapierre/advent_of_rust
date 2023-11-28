adab065280b293ffa6e159e2b068cd5401625b5d
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2023-11-28 16:13:33 +0100

    Create a day selector from the command line

--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -1,3 +1,18 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let day = get_day(&env::args().nth(1));
+
+    println!("Hello, world! Your day is {}", day);
+}
+
+fn get_day(maybe_day: &Option<String>) -> u8 {
+    if let Some(day_str) = maybe_day {
+        day_str
+            .parse()
+            .expect("First given argument to be a string")
+    } else {
+        println!("No day given, defaulting to first day");
+        1
+    }
 }
