51b6d002d7ab44afa59fae8e3b4c608455a3bbbc
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2023-12-02 12:39:23 +0100

    Init of 2023-02

--- src/advent_of_code_2023.rs
+++ src/advent_of_code_2023.rs
@@ -1,7 +1,10 @@
 mod day1;
+mod day2;
+
 pub fn advent_of_code_2023(day: u8) {
     let result = match day {
         1 => day1::solve(),
+        2 => day2::solve(),
         _ => todo!(),
     };
 
