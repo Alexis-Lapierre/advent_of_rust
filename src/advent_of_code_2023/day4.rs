544ec4b2170a597467d61196fc089b86bbed37f4
Alexis Lapierre <128792625+Alexis-Lapierre@users.noreply.github.com>
2023-12-04 15:16:40 +0100

    Solve silver of 2023 day 4

--- src/advent_of_code_2023/day4.rs
+++ src/advent_of_code_2023/day4.rs
@@ -1,9 +1,15 @@
 use std::collections::HashSet;
 
-use crate::aoc_result::AOCResult;
+use crate::{aoc_result::AOCResult, read_file::read_file};
 
 pub fn solve() -> AOCResult {
-    (0, 0).into()
+    let input = read_file(2023, 4).expect("File input/2023/04.txt to exist");
+    let parsed = parse::parse(&input);
+    (silver(parsed), 0).into()
+}
+
+fn silver(lines: impl Iterator<Item = Line>) -> u32 {
+    lines.map(|line| silver_line(&line)).sum()
 }
 
 fn silver_line(line: &Line) -> u32 {
@@ -25,6 +31,8 @@ struct Line {
 mod parse {
     use std::collections::HashSet;
 
+    use nom::{bytes::complete::tag, character::complete::space1};
+
     use super::Line;
 
     pub fn parse(input: &str) -> impl Iterator<Item = Line> + '_ {
@@ -37,21 +45,14 @@ mod parse {
 
     fn line_internal(input: &str) -> nom::IResult<&str, Line> {
         let (input, card) = nom::sequence::delimited(
-            nom::bytes::complete::tag("Card "),
+            nom::sequence::tuple((tag("Card"), space1)),
             nom::character::complete::u8,
-            nom::sequence::tuple((
-                nom::bytes::complete::tag(":"),
-                nom::character::complete::space1,
-            )),
+            nom::sequence::tuple((tag(":"), space1)),
         )(input)?;
 
         let (input, (lucky, winning)) = nom::sequence::separated_pair(
             space_separated_numbers,
-            nom::sequence::tuple((
-                nom::character::complete::space0,
-                nom::bytes::complete::tag("|"),
-                nom::character::complete::space0,
-            )),
+            nom::sequence::tuple((space1, tag("|"), space1)),
             space_separated_numbers,
         )(input)?;
 
@@ -66,16 +67,13 @@ mod parse {
     }
 
     fn space_separated_numbers(input: &str) -> nom::IResult<&str, Vec<u8>> {
-        nom::multi::separated_list1(
-            nom::character::complete::space1,
-            nom::character::complete::u8,
-        )(input)
+        nom::multi::separated_list1(space1, nom::character::complete::u8)(input)
     }
 }
 
 #[cfg(test)]
 mod test {
-    use super::{parse::parse, silver_line};
+    use super::{parse::parse, silver, silver_line};
     const INPUT: &str = "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
 Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
@@ -84,7 +82,7 @@ Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
 Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
 
     #[test]
-    fn test_parse() {
+    fn test_silver_line() {
         let mut lines = parse(INPUT);
         assert_eq!(silver_line(&lines.next().unwrap()), 8);
         assert_eq!(silver_line(&lines.next().unwrap()), 2);
@@ -93,5 +91,11 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11";
         assert_eq!(silver_line(&lines.next().unwrap()), 0);
         assert_eq!(silver_line(&lines.next().unwrap()), 0);
         assert_eq!(lines.next(), None);
+    }
+
+    #[test]
+    fn test_silver() {
+        let lines = parse(INPUT);
+        assert_eq!(silver(lines), 13);
     }
 }
